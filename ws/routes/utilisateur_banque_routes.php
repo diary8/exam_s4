c778fe232aec94f97183b9dacda1d62ce4077bc9
chekalls <146121696+chekalls@users.noreply.github.com>
2025-07-07 16:21:14 +0300

    tatat

---
+++ ws/routes/utilisateur_banque_routes.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../controllers/UtilisateurBanqueController.php';
+require_once __DIR__.'/../db.php';
+
+$db = getDB();
+$utilisateur_banque_controller = new UtilisateurBanqueController($db);
+
+// Flight::route('GET /utilisateur', [$utilisateur_banque_controller, 'getAll']);
+Flight::route('POST /utilisateur/login', [$utilisateur_banque_controller, 'login']);
