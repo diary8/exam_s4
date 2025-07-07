bda915b45ab3f665e0ad8f590dc6ac86ae57e025
chekalls <146121696+chekalls@users.noreply.github.com>
2025-07-07 16:42:58 +0300

    adsasdsd

--- ws/routes/utilisateur_banque_routes.php
+++ ws/routes/utilisateur_banque_routes.php
@@ -6,4 +6,7 @@ $db = getDB();
 $utilisateur_banque_controller = new UtilisateurBanqueController($db);
 
 // Flight::route('GET /utilisateur', [$utilisateur_banque_controller, 'getAll']);
-Flight::route('POST /utilisateur/login', [$utilisateur_banque_controller, 'login']);
+// Flight::route('POST /utilisateur/login', [$utilisateur_banque_controller, 'login']);
+Flight::route('POST /utilisateur/login', function(){
+    Flight::json(["message"=>"ça marche"]);
+});
