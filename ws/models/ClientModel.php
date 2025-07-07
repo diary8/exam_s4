176718fb19e0ba91f682f442980bb8fed5ebf1e0
diary8 <dairy.fitahiana@gmail.com>
2025-07-07 19:19:22 +0300

    liste_client

---
+++ ws/models/ClientModel.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/../db.php';
+
+class ClientModel {
+    public static function getAll() {
+        $db = getDB();
+        $stmt = $db->query("SELECT * FROM client");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function getById($id) {
+        $db = getDB();
+        $stmt = $db->prepare("SELECT * FROM client WHERE id = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+
+    public static function delete($id) {
+        $db = getDB();
+        $stmt = $db->prepare("DELETE FROM client WHERE id = ?");
+        $stmt->execute([$id]);
+    }
+}
